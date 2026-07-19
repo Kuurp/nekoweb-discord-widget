24b6bb70d88cec0255892905b9d3c628c77b7247
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2026-07-19 20:47:34 +0200

    fox

--- update.js
+++ update.js
@@ -39,6 +39,24 @@ async function main() {
     "nekoweb-puddingpudd.json",
     JSON.stringify(output2, null, 2)
   );
+
+  const apiReq3 = await fetch(`https://nekoweb.org/api/site/info/whirrfox.moe`);
+  const apiRes3 = await apiReq3.json();
+
+  const output3 = {
+    domain: apiRes3.domain,
+    title: apiRes3.title,
+    updates: apiRes3.updates,
+    followers: apiRes3.followers,
+    views: apiRes3.views,
+    created_at: new Date(apiRes3.created_at).toLocaleDateString("en-GB"),
+    updated_at: new Date(apiRes3.updated_at).toLocaleDateString("en-GB")
+  };
+
+  fs.writeFileSync(
+    "nekoweb-whirrfox.json",
+    JSON.stringify(output3, null, 2)
+  );
 }
 
 main();
